84bc88d59c54e4a07a25c12b481ffe1a209da353
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-11-30 07:27:08 +0100

    Use heroku url in tests

--- test/app.spec.js
+++ test/app.spec.js
@@ -7,7 +7,7 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('API V1 Routes', () => {
-	let rootUrl = `http://localhost:${process.env.PORT || 3000}/api/v1`;
+	let rootUrl = `https://shrouded-tor-69589.herokuapp.com/api/v1`;
 
 	describe('Red-flags', () => {
 		let baseUrl = `${rootUrl}/red-flags`;
