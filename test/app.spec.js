74908b81914dc0bae7151bbcc80de955c1add0b0
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-12-04 20:50:24 +0100

    chore(test): refactor test file - use chai.request instead of request - import server into test file - remove all unused dependencies from package.json [Finishes #162294709]

--- test/app.spec.js
+++ test/app.spec.js
@@ -1,190 +1,192 @@
-import request from 'request';
 import chai, { expect } from 'chai';
 import chaiHttp from 'chai-http';
+import server from '../src/app';
 
 process.env.NODE_ENV = 'test';
 
 chai.use(chaiHttp);
 
 describe('API V1 Routes', () => {
-  const rootUrl = 'https://glacial-caverns-29493.herokuapp.com/api/v1';
-  // const rootUrl = 'http://localhost:3000/api/v1';
+  const rootUrl = '/api/v1';
 
   describe('Red-flags', () => {
     const baseUrl = `${rootUrl}/red-flags`;
 
     describe('GET /red-flags', () => {
       it('should get all red-flag records', (done) => {
-        request.get(baseUrl, (err, res, body) => {
-          body = JSON.parse(body);
-          expect(res.statusCode).to.eq(200);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(200);
-          body.data.forEach((item) => {
-            expect(item.type).to.eq('red-flag');
+        chai.request(server)
+          .get(baseUrl)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(200);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(200);
+            res.body.data.forEach((item) => {
+              expect(item.type).to.eq('red-flag');
+            });
+            done();
           });
-          done();
-        });
       });
     });
 
     describe('GET /red-flags/:id', () => {
       it('should get a specific red-flag record', (done) => {
-        request.get(`${baseUrl}/1`, (err, res, body) => {
-          body = JSON.parse(body);
-          expect(res.statusCode).to.eq(200);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(200);
-          expect(body.data[0].id).to.eq(1);
-          done();
-        });
+        chai.request(server)
+          .get(`${baseUrl}/1`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(200);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(200);
+            expect(res.body.data[0].id).to.eq(1);
+            done();
+          });
       });
 
       it('should throw error if the record is not found', (done) => {
-        request.get(`${baseUrl}/999`, (err, res, body) => {
-          body = JSON.parse(body);
-          expect(res.statusCode).to.eq(404);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(404);
-          expect(body.data).to.be.undefined;
-          expect(body.error).to.contain('That record does not exist');
-          done();
-        });
+        chai.request(server)
+          .get(`${baseUrl}/999`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(404);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(404);
+            expect(res.body.data).to.be.undefined;
+            expect(res.body.error).to.contain('That record does not exist');
+            done();
+          });
       });
     });
 
     describe('POST /red-flags', () => {
       it('should create a new red-flag record', (done) => {
-        const options = {
-          body: {
-            location: 'Lat: 7.7153986, Lon: 8.5085987',
-            comment: 'Bribery in Federal High Court, Makurdi',
-            createdBy: 1,
-          },
-          json: true,
-          url: baseUrl,
+        const redFlag = {
+          location: 'Lat: 7.7153986, Lon: 8.5085987',
+          comment: 'Bribery in Federal High Court, Makurdi',
+          createdBy: 1,
         };
-        request.post(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(201);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(201);
-          expect(body.data[0].id).not.to.be.undefined;
-          expect(body.data[0].message).to.contain('Created red-flag record');
-          done();
-        });
+        chai.request(server)
+          .post(baseUrl)
+          .send(redFlag)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(201);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(201);
+            expect(res.body.data[0].id).not.to.be.undefined;
+            expect(res.body.data[0].message).to.contain('Created red-flag record');
+            done();
+          });
       });
 
       it('should throw error if the request body is invalid', (done) => {
-        const options = {
-          body: {
-            location: '',
-            createdBy: 1,
-          },
-          json: true,
-          url: baseUrl,
+        const redFlag = {
+          location: '',
+          createdBy: 1,
         };
-        request.post(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(400);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(400);
-          expect(body.data).to.be.undefined;
-          expect(body.error).to.contain('Failed to create record');
-          done();
-        });
+        chai.request(server)
+          .post(baseUrl)
+          .send(redFlag)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(400);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(400);
+            expect(res.body.data).to.be.undefined;
+            expect(res.body.error).to.contain('Failed to create record');
+            done();
+          });
       });
     });
 
     describe('PATCH /red-flags/:id/location', () => {
       it('should update the location of a specific red-flag record', (done) => {
-        const options = {
-          body: { location: 'Lat: 7.7153984, Lon: 8.5085982' },
-          url: `${baseUrl}/1/location`,
-          json: true,
-        };
-        request.patch(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(200);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(200);
-          expect(body.data[0].id).to.eq(1);
-          expect(body.data[0].message).to.eq("Updated red-flag record's location");
-          done();
-        });
+        const location = { location: 'Lat: 7.7153984, Lon: 8.5085982' };
+
+        chai.request(server)
+          .patch(`${baseUrl}/1/location`)
+          .send(location)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(200);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(200);
+            expect(res.body.data[0].id).to.eq(1);
+            expect(res.body.data[0].message).to.eq("Updated red-flag record's location");
+            done();
+          });
       });
 
       it('should throw error if the record is not found', (done) => {
-        const options = {
-          body: { location: 'Lat: 7.7153984, Lon: 8.5085982' },
-          url: `${baseUrl}/999/location`,
-          json: true,
-        };
-        request.patch(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(404);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(404);
-          expect(body.data).to.be.undefined;
-          expect(body.error).to.contain('That record does not exist');
-          done();
-        });
+        const location = { location: 'Lat: 7.7153984, Lon: 8.5085982' };
+
+        chai.request(server)
+          .patch(`${baseUrl}/999/location`)
+          .send(location)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(404);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(404);
+            expect(res.body.data).to.be.undefined;
+            expect(res.body.error).to.contain('That record does not exist');
+            done();
+          });
       });
     });
 
     describe('PATCH /red-flags/:id/comment', () => {
       it('should update the comment of a specific red-flag record', (done) => {
-        const options = {
-          body: { comment: 'Corruption in Makurdi town' },
-          url: `${baseUrl}/1/comment`,
-          json: true,
-        };
-        request.patch(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(200);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(200);
-          expect(body.data[0].id).to.eq(1);
-          expect(body.data[0].message).to.eq("Updated red-flag record's comment");
-          done();
-        });
+        const comment = { comment: 'Corruption in Makurdi town' };
+
+        chai.request(server)
+          .patch(`${baseUrl}/1/comment`)
+          .send(comment)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(200);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(200);
+            expect(res.body.data[0].id).to.eq(1);
+            expect(res.body.data[0].message).to.eq("Updated red-flag record's comment");
+            done();
+          });
       });
 
       it('should throw error if the record is not found', (done) => {
-        const options = {
-          body: { comment: 'Lat: 7.7153984, Lon: 8.5085982' },
-          url: `${baseUrl}/999/comment`,
-          json: true,
-        };
-        request.patch(options, (err, res, body) => {
-          expect(res.statusCode).to.eq(404);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(404);
-          expect(body.data).to.be.undefined;
-          expect(body.error).to.contain('That record does not exist');
-          done();
-        });
+        const comment = { comment: 'Lat: 7.7153984, Lon: 8.5085982' };
+
+        chai.request(server)
+          .patch(`${baseUrl}/999/comment`)
+          .send(comment)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(404);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(404);
+            expect(res.body.data).to.be.undefined;
+            expect(res.body.error).to.contain('That record does not exist');
+            done();
+          });
       });
     });
 
     describe('DELETE /red-flags/:id', () => {
       it('should delete a specific red-flag record', (done) => {
-        request.delete(`${baseUrl}/3`, (err, res, body) => {
-          body = JSON.parse(body);
-          expect(res.statusCode).to.eq(200);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(200);
-          expect(body.data[0].id).to.eq(3);
-          expect(body.data[0].message).to.contain('Deleted red-flag record');
-          done();
-        });
+        chai.request(server)
+          .delete(`${baseUrl}/3`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(200);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(200);
+            expect(res.body.data[0].id).to.eq(3);
+            expect(res.body.data[0].message).to.contain('Deleted red-flag record');
+            done();
+          });
       });
 
       it('should throw error if the record is not found', (done) => {
-        request.delete(`${baseUrl}/999`, (err, res, body) => {
-          body = JSON.parse(body);
-          expect(res.statusCode).to.eq(404);
-          expect(res.headers['content-type']).to.contain('application/json');
-          expect(body.status).to.eq(404);
-          expect(body.data).to.be.undefined;
-          expect(body.error).to.contain('That record does not exist');
-          done();
-        });
+        chai.request(server)
+          .delete(`${baseUrl}/999`)
+          .end((err, res) => {
+            expect(res.statusCode).to.eq(404);
+            expect(res.headers['content-type']).to.contain('application/json');
+            expect(res.body.status).to.eq(404);
+            expect(res.body.data).to.be.undefined;
+            expect(res.body.error).to.contain('That record does not exist');
+            done();
+          });
       });
     });
   });
