fde655f43129a8a555b5099445bb04a593023e50
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-12-07 18:48:16 +0100

    chore(routes): refactor routes - import all routes from index.js [Finishes #162503474]

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import redFlagRoutes from './routes/redFlags';
+import routes from './routes';
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use('/api/v1/red-flags', redFlagRoutes);
+app.use('/api/v1', routes);
 app.all('*', (req, res) => {
   return res.status(404).json({ status: 404, error: 'Not Found!' });
 });
