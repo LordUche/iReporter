9750f66ee4abaf2d08d83a38e1264c6d650997fa
Uchenna A. Iheanacho <uchenna.a.i@gmail.com>
2018-12-13 23:56:49 +0100

    chore(validation): another user cannot edit your red-flag [Finishes #162149182]

--- src/routes/interventions.routes.js
+++ src/routes/interventions.routes.js
@@ -1,16 +1,16 @@
 import { Router } from 'express';
 import controller from '../controllers/interventions.controller';
 import { validateLocation, validateComment, validateStatus } from '../middleware/validators';
-import { isAdmin } from '../middleware/auth';
+import { isAdmin, isNotAdmin, validateOwner } from '../middleware/auth';
 
 const router = new Router();
 
 router.get('/', controller.index);
 router.get('/:id(\\d+)', controller.get);
-router.post('/', validateLocation, validateComment, controller.create);
-router.patch('/:id(\\d+)/location', validateLocation, controller.updateLocation);
-router.patch('/:id(\\d+)/comment', validateComment, controller.updateComment);
+router.post('/', isNotAdmin, validateLocation, validateComment, controller.create);
+router.patch('/:id(\\d+)/location', isNotAdmin, validateOwner, validateLocation, controller.updateLocation);
+router.patch('/:id(\\d+)/comment', isNotAdmin, validateOwner, validateComment, controller.updateComment);
 router.patch('/:id(\\d+)/status', isAdmin, validateStatus, controller.updateStatus);
-router.delete('/:id(\\d+)', controller.delete);
+router.delete('/:id(\\d+)', validateOwner, controller.delete);
 
 export default router;
